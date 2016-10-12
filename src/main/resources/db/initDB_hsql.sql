95c0852c0ff6c3a580396288c1257d844cd2638f
Николай В <gurzufina@yandex.ru>
2016-10-12 14:21:37 +0400

    HW5 base.1

--- src/main/resources/db/initDB_hsql.sql
+++ src/main/resources/db/initDB_hsql.sql
@@ -23,7 +23,8 @@ CREATE TABLE user_roles
   user_id INTEGER NOT NULL,
   role    VARCHAR(255),
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
-  FOREIGN KEY (user_id) REFERENCES USERS (id) ON DELETE CASCADE
+  FOREIGN KEY ( user_id ) REFERENCES USERS (id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE meals
@@ -33,7 +34,8 @@ CREATE TABLE meals
   description VARCHAR(255) NOT NULL,
   calories    INT          NOT NULL,
   user_id     INTEGER      NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES USERS (id) ON DELETE CASCADE
+  FOREIGN KEY ( user_id ) REFERENCES USERS (id)
+    ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX meals_unique_user_datetime_idx
   ON meals (user_id, date_time)
