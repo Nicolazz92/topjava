08d829e9184d8f97bfa48b2cc1be7305614a7aba
Николай В <gurzufina@yandex.ru>
2016-10-17 12:40:28 +0400

    hw6 1.1

---
+++ src/test/java/ru/javawebinar/topjava/service/mock/MockMealServiceTest.java
@@ -0,0 +1,8 @@
+package ru.javawebinar.topjava.service.mock;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.AbstractMealServiceTest;
+
+@ActiveProfiles("mock")
+public class MockMealServiceTest extends AbstractMealServiceTest {
+}
